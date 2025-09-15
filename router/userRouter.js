5f6aae57ef83aa589225f8c162d38838283312ca
OkoyeAlfred <ekeneokoye93@gmail.com>
2025-09-15 16:46:08 +0100

    i did get one

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,8 +1,9 @@
-const {register} = require("../controllers/userController");
+const {register, getOne} = require("../controllers/userController");
 const upload = require("../middleware/multer");
 
 const userRouter = require("express").Router();
 
 userRouter.post("/register",upload.single("profilePicture"), register);
+userRouter.get("/get-one", getOne)
 
 module.exports = userRouter
